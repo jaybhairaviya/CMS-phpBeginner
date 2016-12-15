d970eb65a11a827d4c3ae3d32af2374e357f6a36
jaybhairaviya <jay.bhairaviya@gmail.com>
2016-12-15 13:26:30 +0530

    Minor Visual bug fix

--- admin/comments.php
+++ admin/comments.php
@@ -21,7 +21,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Posts
+                            Comments
                         </h1>
 
 
